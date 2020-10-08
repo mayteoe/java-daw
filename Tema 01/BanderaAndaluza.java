6d24a853f2f678e0d371a2931f08ae0a76dea387
Pepito <profesorjim@gmail.com>
2020-10-08 10:31:43 +0200

    actualizacion

---
+++ Tema 01/BanderaAndaluza.java
@@ -0,0 +1,16 @@
+public class BanderaAndaluza {
+  public static void main (String[] args) {
+    
+    // formato: color - espacios - color
+    System.out.printf ("%s%20s%s\n","\033[42m","","\033[40m");
+    System.out.printf ("%s%20s%s\n","\033[42m","","\033[40m");
+    System.out.printf ("%s%20s%s\n","\033[107m","","\033[40m");
+    System.out.printf ("%s%20s%s\n","\033[107m\033[90m","ANDALUCÍA      ","\033[40m");
+    //System.out.printf ("%s%20s%s\n","\033[107m","ANDALUCÍA      ","\033[40m");
+    System.out.printf ("%s%20s%s\n","\033[107m","","\033[40m");
+    System.out.printf ("%s%20s%s\n","\033[42m","","\033[40m");
+    System.out.printf ("%s%20s%s\n","\033[42m","","\033[40m"); 
+    
+    
+  }
+}
