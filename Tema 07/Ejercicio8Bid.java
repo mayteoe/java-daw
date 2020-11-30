a6b59a9c05e0e84875b2f244cea71e31775ec1f1
Pepito <profesorjim@gmail.com>
2020-11-30 10:07:03 +0100

    actualizacion

---
+++ Tema 07/Ejercicio8Bid.java
@@ -0,0 +1,32 @@
+public class Ejercicio8Bid {
+  public static void main (String [] args) {
+    String coordenada;
+    int fila, columna;
+    
+    System.out.print("Introduzca la posición del alfin: ");
+    coordenada = System.console().readLine();
+    
+    columna = coordenada.charAt(0) - 'a';
+    fila = coordenada.charAt(1) - '1';
+    
+    System.out.println("El alfil puede moverse a las siguientes posiciones:");
+    
+    int diferenciaFilas, diferenciaColumnas;
+    
+    for (int i=0; i<8; i++) {
+      for (int j=0; j<8; j++) {
+        diferenciaFilas = i-fila;
+        if (diferenciaFilas<0) {
+          diferenciaFilas = diferenciaFilas*(-1);
+        }          
+        diferenciaColumnas = j-columna;
+        if (diferenciaColumnas<0) {
+          diferenciaColumnas = diferenciaColumnas*(-1);
+        }          
+        if (diferenciaColumnas == diferenciaFilas && i!=fila && j!=columna){
+          System.out.print(""+(char)(j+'a')+(i+1)+" ");
+        }
+      }
+    }    
+  }
+}
