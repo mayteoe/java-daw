79b129d3ebd2cee6304eecb49d54a54a979b76fb
Pepito <profesorjim@gmail.com>
2020-12-09 08:55:55 +0100

    actualizacion

--- Tema 03/PruebaPrintsYScanner.java
+++ Tema 03/PruebaPrintsYScanner.java
@@ -1,13 +1,21 @@
-public class PruebaPrints {
+import java.util.Scanner;
+
+public class PruebaPrintsYScanner {
   public static void main (String[] args) {
     int variableEntera = 457;
     float variableFlotante = (float)3.4;
     long variableEnteraLarga = 45;
     double variableFlotanteLarga = 13.344645645655;
+    int vE;
+    
+    Scanner s = new Scanner(System.in);
     
     System.out.printf("%d %f %d %f\n",variableEntera,variableFlotante,variableEnteraLarga,variableFlotanteLarga);
     System.out.printf("%2d %10.2f %5d %5.1f\n",variableEntera,variableFlotante,variableEnteraLarga,variableFlotanteLarga);
     System.out.printf("%2d %10.2f %5d %.2f\n",variableEntera,variableFlotante,variableEnteraLarga,variableFlotanteLarga);
+   
+      
+    vE = s.nextInt();
     
   }
   
