490b12d7324b539f0811e78e8941483fef167d15
Pepito <profesorjim@gmail.com>
2021-01-27 09:55:13 +0100

    actualizacion

---
+++ Tema 08/EjemploPaquetizacion3/Programa/ProgramaPrincipal.java
@@ -0,0 +1,12 @@
+package Programa;
+import LibreriaFunsAuxs.FuncionesAuxiliares;
+
+public class ProgramaPrincipal {
+  public static void main (String[] args) {
+    
+    System.out.println(FuncionesAuxiliares.suma(10,20));
+    
+    System.out.println(FuncionesAuxiliares.resta(10,20));
+    
+  }
+}
